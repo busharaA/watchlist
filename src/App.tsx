58d5f0c6ec0aa2f019f43f9dd43f1f6da952f1dd
aBushara <aisha.bushara@gmail.com>
2023-08-28 20:57:14 +0200

    Handled browser back or forward button event

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header";
 import { useAppDispatch, useAppSelector } from "./app/hooks";
-import { selectStatus, selectQuery, fetchBySearchQuery } from "./features/shows/showsSlice";
+import { selectStatus, selectQuery, fetchBySearchQuery, setSearchQuery } from "./features/shows/showsSlice";
 import Loader from "./components/loader";
-import { useEffect, /*useState*/ } from "react";
+import { useEffect } from "react";
 import SearchBar from "./containers/search";
 import RemoveModal from "./components/remove_modal";
 import { selectShowModal, selectShowNotification } from "./features/rmModal/modalSlice";
 import RemovalNotification from "./components/removal_notification";
-// import { useLocation } from "react-router-dom";
-
-// const useBackButton = () => {
-//     const [isBack, setIsBack] = useState(false);
-//     const handleGoBack = () => {
-//         setIsBack(true);
-//     };
-
-//     useEffect(() => {
-//         window.addEventListener("popstate", handleGoBack);
-//         return () => window.removeEventListener("popstate", handleGoBack);
-//     });
-
-//     return isBack;
-// }
+import { useLocation } from "react-router-dom";
 
 const App = () => {
     const status = useAppSelector(selectStatus);
@@ -30,26 +16,20 @@ const App = () => {
     const showRmModal = useAppSelector(selectShowModal);
     const showNotification = useAppSelector(selectShowNotification);
     const dispatch = useAppDispatch();
-    // const isBack = useBackButton();
-    // const location = useLocation();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.search !== "") {
+            dispatch(setSearchQuery(location.search.replace("?q=", "")));
+        }
+        // Handles restoring previous search results by clicking browser back/forward button
+    }, [dispatch, location]);
 
     useEffect(() => {
         if (query !== "") {
             dispatch(fetchBySearchQuery(query));
         }
     }, [query, dispatch]);
-
-    //TODO: AFTER COMMIT: Handle returning to base url (or one query back) when a user clicks "back" 
-    // if returning one query back, fetch previous data from API using query part of url
-
-    // console.log(isBack);
-    // console.log(location.search);
-    // useEffect(() => {
-    //     if (isBack) {
-    //         // dispatch(fetchBySearchQuery());
-    //         console.log(location.search);
-    //     }
-    // });
 
     return (
         <div className="App">
